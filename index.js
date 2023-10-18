3b3a74df8632f242929b77b64497fea8d7775e8d
Roaawan <roaafatani@outlook.com>
2023-10-18 15:28:43 +0300

    connected to database

--- index.js
+++ index.js
@@ -4,19 +4,30 @@ const app = express();
 
 // Connect to db
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://roaafatani:p2B0O5TZhnDxIkQ6@cluster0.wumgsif.mongodb.net/waaterProject?retryWrites=true&w=majority")
+const uri = "mongodb+srv://roaafatani:p2B0O5TZhnDxIkQ6@cluster0.wumgsif.mongodb.net/waaterProject?retryWrites=true&w=majority"
 
 // Import user model
-const UserRecordModel = require('./models/UserRecord')
+// const UserRecordModel = require('./models/UserRecord')
 
 // app.get('/', (req, res) => {
 //     res.json({"users":["Roaa","Afnan","Amjad","summaya"]});
 // });
 
-app.get('/usersrecord', async (req, res) => {
-    const usersrecord = await UserRecordModel.find();
-    res.json(usersrecord)
-});
+// app.get('/usersrecord', async (req, res) => {
+//     const usersrecord = await UserRecordModel.find();
+//     res.json(usersrecord)
+// });
+
+async function connect() {
+    try{
+        await mongoose.connect(uri)
+        console.log("Connected to mongoDB")
+    }catch (error){
+        console.error(error);
+    }
+}
+
+connect();
 
 app.listen(3000,() => {
     console.log("Server started on port 3000")
